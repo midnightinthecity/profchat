2e9bbb641eae1f30c1295a3bb8de9cc71696299f
midnightinthecity <midnightinthecity@gmail.com>
2019-04-08 09:28:57 -0400

    cleanup

--- js/main.js
+++ js/main.js
@@ -2,16 +2,12 @@ $(function() {
   let getNewMessages = function() {
     $.get('.netlify/functions/messages',function(data){
       let messages = JSON.parse(data).sort((a, b) => (a['number'] - b['number']))
-      console.log(messages)
       let $messageDiv = $('<div>')
       for (let message of messages) {
-        let messageText = `<p>${message.data.who}: ${message.data.message}</p>`
-        $messageDiv.append($(messageText))
-        console.log(messageText)
-        console.log($messageDiv)
+        let $p = $(`<p>${message.data.who}: ${message.data.message}</p>`)
+        $messageDiv.append($p)
       }
       $('#messages').empty().append($messageDiv)
-      console.log($('#messages'))
     })
   }
 
