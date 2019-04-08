58dd7c2d06f37168439ac27c98865d9f2274ee13
midnightinthecity <midnightinthecity@gmail.com>
2019-04-08 09:11:39 -0400

    jquery wrapper

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-let getNewMessages = function() {
-  $.get('.netlify/functions/messages',function(data){
-    let messages = JSON.parse(data).sort((a, b) => (a['number'] - b['number']))
-    let $messageDiv = $('div')
-    for (let message of messages) {
-      let messageText = `<p>${message.data.who}: ${message.data.message}</p>`
-      $messageDiv.append(messageText)
-    }
-    $('#messages').empty().append($messageDiv)
-  })
-}
+$(function() {
+  let getNewMessages = function() {
+    $.get('.netlify/functions/messages',function(data){
+      let messages = JSON.parse(data).sort((a, b) => (a['number'] - b['number']))
+      console.log(messages)
+      let $messageDiv = $('div')
+      for (let message of messages) {
+        let messageText = `<p>${message.data.who}: ${message.data.message}</p>`
+        $messageDiv.append(messageText)
+      }
+      $('#messages').empty().append($messageDiv)
+    })
+  }
 
-getNewMessages()
+  getNewMessages()
 
-setInterval(getNewMessages, 10000)
+  setInterval(getNewMessages, 10000)
+})
+
