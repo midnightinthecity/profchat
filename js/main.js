e1225a080223c225f1e40b623f4c8cef22137b6e
midnightinthecity <midnightinthecity@gmail.com>
2019-04-08 09:23:27 -0400

    div fix

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function() {
     $.get('.netlify/functions/messages',function(data){
       let messages = JSON.parse(data).sort((a, b) => (a['number'] - b['number']))
       console.log(messages)
-      let $messageDiv = $('div')
+      let $messageDiv = $('<div>')
       for (let message of messages) {
         let messageText = `<p>${message.data.who}: ${message.data.message}</p>`
         $messageDiv.append($(messageText))
